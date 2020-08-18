02f172e4b0de371f489af4a8576058c4804817af
Tim Bourguignon <timothee.bourguignon@mathema.de>
2020-08-18 12:12:41 +0200

    Modified the createguest script to embolden the name right away

--- CreateGuest.py
+++ CreateGuest.py
@@ -2,18 +2,26 @@
 import sys
 import os 
 
+# Usage
+# CreateGuest.py '114' 'Kemdi Ebi' 'Nov 04' 'title_without_number'
+
 def loadIndex(path):
     indexFile = open(os.path.dirname(os.path.realpath(__file__)) + "\\" + path,'r')
     return indexFile.read()
     
 def CreateFile(pagecontent, args):
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    path = dir_path + "\\Guests\\" + args[1] + "-" + args[2] + args[3] + ".html"
+    # FileFormat Number_FirstNameLastName.html"
+    number = args[1]
+    nameWOSpaces = args[2].replace(" ", "")
+
+    path = dir_path + "\\Guests\\" + number + "-" + nameWOSpaces + ".html"
     htmlFile = open(path, 'w')
     htmlFile.write(pagecontent)
     htmlFile.close()
 
-    pathTx = dir_path + "\\Guests\\Transcripts\\" + args[1] + "-" + args[2] + args[3] + "Tx.html"
+    # FileFormat Number_FirstNameLastNameTx.html"
+    pathTx = dir_path + "\\Guests\\Transcripts\\" + number + "-" + nameWOSpaces + "Tx.html"
     htmlTxFile = open(pathTx, 'w')
     htmlTxFile.write("<span></span>")
     htmlTxFile.close()
@@ -23,11 +31,11 @@ def overwriteFile(path, content):
     htmlFile.write(content)
     htmlFile.close()
 
-if(len(sys.argv) != 6):
-    print("CreateGuest.py 74 Kemdi Ebi 'Nov 04' 'title_without_#'")
+if(len(sys.argv) != 5):
+    print("CreateGuest.py '114' 'Kemdi Ebi' 'Nov 04' 'title_without_number'")
     exit()
 
-print("Creating pages for Guest: " + sys.argv[2] + " " + sys.argv[3])
+print("Creating pages for Guest: " + sys.argv[2])
 
 GUEST_PAGE = """---
 layout: single
@@ -56,21 +64,31 @@ description: #TITLE_WITHOUT_HASH
 {% include disqus.html %}
 {% endif %}"""
 
-GUEST_PAGE = GUEST_PAGE.replace("NUMBER", sys.argv[1])
-GUEST_PAGE = GUEST_PAGE.replace("FIRSTNAME", sys.argv[2])
-GUEST_PAGE = GUEST_PAGE.replace("LASTNAME", sys.argv[3])
-GUEST_PAGE = GUEST_PAGE.replace("TITLE_WITHOUT_HASH", sys.argv[5])
+number = sys.argv[1]
+guestname = sys.argv[2]
+firstname = sys.argv[2].split(" ")[0]
+lastname = sys.argv[2].split(" ")[1]
+date = sys.argv[3]
+title = sys.argv[4]
+
+GUEST_PAGE = GUEST_PAGE.replace("NUMBER", number)
+GUEST_PAGE = GUEST_PAGE.replace("FIRSTNAME", firstname)
+GUEST_PAGE = GUEST_PAGE.replace("LASTNAME", lastname)
+GUEST_PAGE = GUEST_PAGE.replace("TITLE_WITHOUT_HASH", number + " " + title)
 
 CreateFile(GUEST_PAGE, sys.argv)
 
 SLUG_TO_FIND = "<!-- insert here -->"
 SLUG_TO_REPLACE = """<!-- insert here -->
         <li>DATE, 2020: <a href="/Guests/NUMBER-FIRSTNAMELASTNAME.html">#TITLE_WITHOUT_HASH</a></li>"""
-SLUG_TO_REPLACE = SLUG_TO_REPLACE.replace("NUMBER", sys.argv[1])
-SLUG_TO_REPLACE = SLUG_TO_REPLACE.replace("FIRSTNAME", sys.argv[2])
-SLUG_TO_REPLACE = SLUG_TO_REPLACE.replace("LASTNAME", sys.argv[3])
-SLUG_TO_REPLACE = SLUG_TO_REPLACE.replace("DATE", sys.argv[4])
-SLUG_TO_REPLACE = SLUG_TO_REPLACE.replace("TITLE_WITHOUT_HASH", sys.argv[5])
+SLUG_TO_REPLACE = SLUG_TO_REPLACE.replace("NUMBER", number)
+SLUG_TO_REPLACE = SLUG_TO_REPLACE.replace("FIRSTNAME", firstname)
+SLUG_TO_REPLACE = SLUG_TO_REPLACE.replace("LASTNAME", lastname)
+SLUG_TO_REPLACE = SLUG_TO_REPLACE.replace("DATE", date)
+
+emboldenedTitle = title.replace(guestname, "<b>" + guestname + "</b>")
+
+SLUG_TO_REPLACE = SLUG_TO_REPLACE.replace("TITLE_WITHOUT_HASH", number + " " + emboldenedTitle)
 
 content = loadIndex("index.html")
 content = content.replace(SLUG_TO_FIND, SLUG_TO_REPLACE)
