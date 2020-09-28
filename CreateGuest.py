e50a200107538c36769726756bc4fe2dfaeca11b
Tim Bourguignon <timothee.bourguignon@mathema.de>
2020-09-28 22:35:00 +0200

    Added Dinky Theme

--- CreateGuest.py
+++ CreateGuest.py
@@ -38,7 +38,7 @@ if(len(sys.argv) != 5):
 print("Creating pages for Guest: " + sys.argv[2])
 
 GUEST_PAGE = """---
-layout: single
+layout: default
 comments: true
 title: #NUMBER FIRSTNAME LASTNAME
 description: #TITLE_WITHOUT_HASH
