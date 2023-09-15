bac334f666ec24035a451c08d97af6e05d009c7e
Tim Bourguignon <timothepdotnet@gmail.com>
2023-09-15 09:19:01 +0200

    Fixing Madisons metadata

--- CreateGuest.py
+++ CreateGuest.py
@@ -43,7 +43,10 @@ GUEST_PAGE = """---
 layout: default
 comments: true
 title: "#NUMBER FIRSTNAME LASTNAME's Software Developer's Journey interview"
-description: "#TITLE_WITHOUT_HASH"
+description: "#DESCRIPTION"
+keywords: "KEYWORDS"
+datePublished: "DATE"
+episodeNumber: "NUMBER"
 ---
 <h1>#TITLE_WITHOUT_HASH</h1>
 <script type='text/javascript' charset='utf-8' src='https://www.buzzsprout.com/190346.js?player=small&artist=Timoth%C3%A9e%20Bourguignon,%20FIRSTNAME%20LASTNAME'></script>
